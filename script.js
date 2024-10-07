dec5a2c6eebc9b9ce8980a88c8256de4ed8bce53
hasnian1122 <soomronain22@gmail.com>
2024-10-07 02:19:20 -0700

    arrloop

---
+++ script.js
@@ -0,0 +1,79 @@
+// print 1 to 10
+// for (var i = 1; i <= 10; i++){
+// document.write(i);
+// }
+
+
+// this is heading
+// document.write("Hello peep")
+
+
+// table of any number
+// var num = prompt("enter a number");
+// for (let i = 1; i <= 10; i++) {
+//     console.log(`${num} x ${i} = ${num * i}`);
+// }
+
+
+// table of any number with lenth
+// var num = prompt("enter a number");
+// var num2 = prompt("enter lenth of table");
+// for (let i = 1; i <= num2; i++) {
+//     console.log(`${num} x ${i} = ${num * i}`);
+// }
+
+
+// fruit array 
+// var  fruit = ["apple",  "banana" , "mango" , "orange" , "strwaberry"];
+//     for (let i = 0; i <= fruit.length; i++) {
+//         console.log(fruit[i]);
+        
+//     }
+
+
+// counting
+// for (let i = 0; i <= 15; i++) {
+//     console.log(i);   
+// }
+
+
+// reverse  counting
+// for (let i = 10; i >= 1; i--) {
+//     console.log(i);   
+// }
+
+
+// even counting
+// let evenNumbers = "Even: ";
+
+// for (let i = 0; i <= 20; i++) { 
+//     if (i % 2 === 0) {
+//         evenNumbers += i + " ";
+//     }
+// }
+
+// console.log(evenNumbers);
+
+
+// // even counting
+// let oddNumbers = "Odd: ";
+
+// for (let i = 0; i <= 20; i++) { 
+//     if (i % 2 !== 0) {
+//         oddNumbers += i + " ";
+//     }
+// }
+
+// console.log(oddNumbers);
+
+
+// series 2k 4k 6k .... up to 20k
+// let evenNumbers = "Even: ";
+
+// for (let i = 2; i <= 20; i += 2) {  
+//     evenNumbers += i + "k ";  
+// }
+
+// console.log(evenNumbers);
+
+
